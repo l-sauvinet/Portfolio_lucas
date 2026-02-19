7ebf05b3dbaffef6ee82e50cf6935e40c16f2f20
Lucas <lucasovinet@gmail.com>
2026-02-19 11:57:24 +0100

    feat: header

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Header from "./components/Header.vue";
+import Description from "./components/Description.vue";
+import Project from "./components/Project.vue";
+import Synthese from "./components/Synthese.vue";
+import Veille from "./components/Veille.vue";
+</script>
+
+<template>
+  <div id="app">
+    <Header />
+    <Description />
+    <Project />
+    <Synthese />
+    <Veille />
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #f0f0f0;
+}
+
+#app {
+   font-family: 'Montserrat', sans-serif;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,64 @@
+<template>
+  <header class="site-header">
+    <div class="container">
+      <a class="logo-link" href="/">
+        <img src="/assets/logo-p.svg" alt="Logo" class="logo" />
+      </a>
+
+      <nav class="main-nav" aria-label="Main navigation">
+        <a href="#accueil">Accueil</a>
+        <a href="#description">Description</a>
+        <a href="#projet">Projet</a>
+        <a href="#veille">Veille</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.site-header {
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 25px;
+  box-sizing: border-box;
+  box-shadow: 2px 4px 12px rgba(0,0,0,0.15);
+  position: relative;
+}
+
+.container {
+  width: 100%;
+  padding: 0 50px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.logo {
+  height: 75px;
+  display: block;
+}
+
+.main-nav {
+  display: flex;
+  gap: 20px;
+  align-items: center;
+}
+
+.main-nav a {
+  color: #0C3808;
+  text-decoration: none;
+  font-size: 20px;
+  padding: 6px 8px;
+}
+
+.main-nav a:hover {
+  color: #298421;
+}
+</style>
